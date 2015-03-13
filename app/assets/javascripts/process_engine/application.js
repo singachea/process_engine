5a51a2ec79e1e166b5ac73261a64ab586923fd25
Ream <singachea@gmail.com>
2015-03-13 16:39:58 +0800

    fully porting diagram

--- app/assets/javascripts/process_engine/application.js
+++ app/assets/javascripts/process_engine/application.js
@@ -14,3 +14,52 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+
+function loadBMPNViewer(bpmnXMLUrl, highlightedNodes, BpmnViewer, $) {
+
+  // create viewer
+  var bpmnViewer = new BpmnViewer({
+    container: '#bpmn_canvas'
+  });
+
+
+  // import function
+  function importXML(xml) {
+
+    // import diagram
+    bpmnViewer.importXML(xml, function(err) {
+
+      if (err) {
+        return console.error('could not import BPMN 2.0 diagram', err);
+      }
+
+      var canvas = bpmnViewer.get('canvas'),
+          overlays = bpmnViewer.get('overlays');
+
+
+      // zoom to fit full viewport
+      canvas.zoom('fit-viewport');
+
+      // attach an overlay to a node
+      // overlays.add('decision', 'note', {
+      //   position: {
+      //     bottom: 0,
+      //     right: 0
+      //   },
+      //   html: '<div class="diagram-note">Mixed up the labels?</div>'
+      // });
+
+      // add marker
+      // canvas.addMarker('decision', 'bpmn-highlight-node');
+      // canvas.addMarker('UserTask_1', 'bpmn-highlight-node');
+      for(var i in highlightedNodes) {
+        canvas.addMarker(highlightedNodes[i], 'bpmn-highlight-node');
+      }
+    });
+  }
+
+
+  // load external diagram file via AJAX and import it
+  $.get(bpmnXMLUrl, importXML, 'text');
+}
